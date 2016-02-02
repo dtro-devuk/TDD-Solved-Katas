863721390794628d4cdebd6b71060d1bf3d8f260
dtro-devuk <dtro-devuk@users.noreply.github.com>
2016-02-02 17:13:56 +0000

    TDD Fizz Buzz - test input up to 3

---
+++ Tests/TDD.Katas.CSharp.Test.Unit/FizzBuzzTests/FizzBuzzMinMaxInputTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace TDD.Katas.CSharp.Test.Unit.FizzBuzzTests
+{
+    [Category("Fizz Buzz Kata")]
+    public class FizzBuzzMinMaxInputTests : WhenTestingFizzBuzz
+    {
+        #region Test Methods
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        public void GivenNumberBelowMinimumInput_ReturnEmptyString(int actualInput)
+        {
+            ActAndAssert();
+            Assert.That(_resultFizz, Is.Empty);
+        }
+
+
+        [TestCase(101)]
+        public void GivenNumberAboveMaxInput_ReturnEmptyString(int actualInput)
+        {
+            ActAndAssert();
+            Assert.That(_resultFizz, Is.Empty);
+        }
+        #endregion
+    }
+}
---
+++ Tests/TDD.Katas.CSharp.Test.Unit/FizzBuzzTests/TestASingleNumber.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace TDD.Katas.CSharp.Test.Unit.FizzBuzzTests
+{
+    [Category("Fizz Buzz Kata")]
+    public class TestASingleNumber : WhenTestingFizzBuzz
+    {
+        public void Arrange()
+        {
+            ActualInputValue = 1;
+        }
+
+        [Test]
+        public void GivenValidSingleNumber_ItShouldReturnAStringNumber()
+        {
+            Arrange();
+            ActAndAssert();
+            Assert.AreEqual("1", _resultFizz);
+        }
+
+        [TestCase(1,"1")]
+        [TestCase(2, "2")]
+        public void GivenValidSingleNumbersBelowTwo_ItShouldReturnTheCorrectStringNumber(int actual, string expectedResult)
+        {
+            ActualInputValue = actual;
+
+            ActAndAssert();
+            Assert.AreEqual(expectedResult, _resultFizz);
+        }
+
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "FIZZ")]
+        public void GivenValidSingleNumbersBelowThree_ItShouldReturnTheCorrectString(int actual, string expectedResult)
+        {
+            ActualInputValue = actual;
+
+            ActAndAssert();
+            Assert.AreEqual(expectedResult, _resultFizz);
+        }
+    }
+}
